1fb36a025f97e1f998e3d35101518fc8533e7d5f
周张林 <zhouzhanglin@gmail.com>
2017-05-01 09:26:14 +0800

    Update Light Test Demo

--- Unity/Demos/Assets/Scripts/Demos/LightTest.cs
+++ Unity/Demos/Assets/Scripts/Demos/LightTest.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class LightTest : MonoBehaviour {
+	public Transform target;
 
 	// Use this for initialization
 	void Start () {
@@ -10,5 +11,6 @@ public class LightTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.RotateAround(new Vector3(1.2f,1.2f,-0.5f),Vector3.forward,4);
+		target.localEulerAngles = new Vector3(Mathf.Sin(Time.realtimeSinceStartup)*10+5,0f,0f);
 	}
 }
