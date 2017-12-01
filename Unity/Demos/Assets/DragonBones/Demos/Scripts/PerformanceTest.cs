628a2d589cba443669a74bb77f4be01bd4a6862c
xuliang <wuliao2230@foxmail.com>
2017-12-01 16:26:39 +0800

    update

--- Unity/Demos/Assets/DragonBones/Demos/Scripts/PerformanceTest.cs
+++ Unity/Demos/Assets/DragonBones/Demos/Scripts/PerformanceTest.cs
@@ -1,27 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using DragonBones;
 
 namespace performanceTest
 {
     public class PerformanceTest : MonoBehaviour
     {
-		public UnityDragonBonesData dragonBoneData;
+        public UnityDragonBonesData dragonBoneData;
 
         void Start()
         {
-			UnityFactory.factory.LoadData(dragonBoneData);
+            UnityFactory.factory.LoadData(dragonBoneData);
 
-            int lY = 20;
-            int lX = 40;
+            StartCoroutine(BuildArmatureComponent());
+        }
 
+        IEnumerator BuildArmatureComponent()
+        {
+            int lY = 10;
+            int lX = 10;
             for (var y = 0; y < lY; ++y)
             {
                 for (var x = 0; x < lX; ++x)
                 {
-					var armatureComponent = UnityFactory.factory.BuildArmatureComponent("DragonBoy");
+                    var position = new Vector3((x - lX * 0.5f) * 1.0f, (y - lY * 0.5f) * 1.0f, x + lX * y * 0.01f);
+
+                    var armatureComponent = UnityFactory.factory.BuildArmatureComponent("DragonBoy");
                     armatureComponent.armature.cacheFrameRate = 24; // Cache animation.
                     armatureComponent.animation.Play("walk");
-                    armatureComponent.transform.localPosition = new Vector3((x - lX * 0.5f) * 1.0f, (y - lY * 0.5f) * 1.0f, x + lX * y * 0.01f);
+                    armatureComponent.transform.localPosition = position;
+
+                    yield return new WaitForSecondsRealtime(1.0f);
                 }
             }
         }
