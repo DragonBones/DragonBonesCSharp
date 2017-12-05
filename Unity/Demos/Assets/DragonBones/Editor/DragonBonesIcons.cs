d8b77e80f8bc217d4351fbf29ed85a55aba01313
xuliang <wuliao2230@foxmail.com>
2017-12-05 10:31:55 +0800

    fixed UnityBone Transform

--- Unity/Demos/Assets/DragonBones/Editor/DragonBonesIcons.cs
+++ Unity/Demos/Assets/DragonBones/Editor/DragonBonesIcons.cs
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 using System.Reflection;
@@ -32,9 +32,9 @@ namespace DragonBones
     public class DragonBonesIcons : Editor
     {
 
-        static Texture2D /*textureBone,*/textureArmature,textureImg,textureMesh/*,textureIk*/;
-        static string editorPath="";
-        static string editorGUIPath=  "";
+        static Texture2D textureBone, textureArmature, textureImg, textureMesh/* , textureIk */;
+        static string editorPath = "";
+        static string editorGUIPath = "";
         static bool isInited = false;
         static DragonBonesIcons()
         {
@@ -49,22 +49,22 @@ namespace DragonBones
             }
 
             DirectoryInfo rootDir = new DirectoryInfo(Application.dataPath);
-            FileInfo[] files = rootDir.GetFiles("DragonBonesIcons.cs", SearchOption.AllDirectories); 
+            FileInfo[] files = rootDir.GetFiles("DragonBonesIcons.cs", SearchOption.AllDirectories);
             editorPath = Path.GetDirectoryName(files[0].FullName.Replace("\\", "/").Replace(Application.dataPath, "Assets"));
-            editorGUIPath = editorPath + "/GUI"; 
+            editorGUIPath = editorPath + "/GUI";
 
-            //textureBone = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath+"/icon-bone.png");
-            //textureIk = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath+"/icon-ik.png");
-            textureArmature = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath+"/icon-skeleton.png");
-            textureImg = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath+"/icon-image.png");
-            textureMesh = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath+"/icon-mesh.png");
+            textureBone = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath + "/icon-bone.png");
+            //textureIk = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath + "/icon-ik.png");
+            textureArmature = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath + "/icon-skeleton.png");
+            textureImg = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath + "/icon-image.png");
+            textureMesh = AssetDatabase.LoadAssetAtPath<Texture2D>(editorGUIPath + "/icon-mesh.png");
 
             EditorApplication.hierarchyWindowItemOnGUI -= HierarchyIconsOnGUI;
             EditorApplication.hierarchyWindowItemOnGUI += HierarchyIconsOnGUI;
             isInited = true;
         }
 
-        static void HierarchyIconsOnGUI (int instanceId, Rect selectionRect)
+        static void HierarchyIconsOnGUI(int instanceId, Rect selectionRect)
         {
             GameObject go = (GameObject)EditorUtility.InstanceIDToObject(instanceId);
             if (!go)
@@ -72,59 +72,57 @@ namespace DragonBones
                 return;
             }
 
-            Rect rect = new Rect(selectionRect.x-25f, selectionRect.y+2, 15f, 15f);
+            Rect rect = new Rect(selectionRect.x - 25f, selectionRect.y + 2, 15f, 15f);
 
-            if(go.GetComponent<UnityArmatureComponent>())
+            if (go.GetComponent<UnityArmatureComponent>())
             {
-                rect.x =  selectionRect.x+ selectionRect.width - 15f;
-                GUI.Label(rect,textureArmature);
+                rect.x = selectionRect.x + selectionRect.width - 15f;
+                GUI.Label(rect, textureArmature);
                 return;
             }
 
             UnityUGUIDisplay ugui = go.GetComponent<UnityUGUIDisplay>();
-            if(ugui && ugui.sharedMesh)
+            if (ugui && ugui.sharedMesh)
             {
-                if(ugui.sharedMesh.vertexCount==4)
+                if (ugui.sharedMesh.vertexCount == 4)
                 {
-                    GUI.Label(rect,textureImg);
+                    GUI.Label(rect, textureImg);
                 }
                 else
                 {
-                    GUI.Label(rect,textureMesh);
+                    GUI.Label(rect, textureMesh);
                 }
                 return;
             }
 
             MeshFilter mf = go.GetComponent<MeshFilter>();
-            if(mf && mf.sharedMesh && 
-                mf.transform.parent!=null &&
+            if (mf && mf.sharedMesh &&
+                mf.transform.parent != null &&
                 mf.transform.parent.GetComponent<UnityArmatureComponent>() != null)
-                //mf.transform.parent.parent!=null &&
-                //mf.transform.parent.parent.GetComponent<UnityArmatureComponent>()!=null)
             {
-                if(mf.sharedMesh.vertexCount==4)
+                if (mf.sharedMesh.vertexCount == 4)
                 {
-                    GUI.Label(rect,textureImg);
+                    GUI.Label(rect, textureImg);
                 }
                 else
                 {
-                    GUI.Label(rect,textureMesh);
+                    GUI.Label(rect, textureMesh);
                 }
                 return;
             }
 
-            /*UnityBone ubone = go.GetComponent<UnityBone>();
-            if(ubone)
+            UnityBone ubone = go.GetComponent<UnityBone>();
+            if (ubone)
             {
                 Texture2D icon = textureBone;
                 Bone bone = ubone.bone;
-                if(bone!=null && bone.ik!=null){
-                {
-                    icon = textureIk;
-                }
-                GUI.Label(rect,icon);
+                // if (bone != null && bone.ik != null)
+                // {
+                //     icon = textureIk;
+                // }
+                GUI.Label(rect, icon);
                 return;
-            }*/
+            }
         }
     }
 
