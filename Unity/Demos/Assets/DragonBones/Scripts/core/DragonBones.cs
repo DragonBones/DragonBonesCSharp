c86c27bc775bb3248533ffa5b9c4ca4d8998c33c
xuliang <wuliao2230@foxmail.com>
2018-01-09 11:21:29 +0800

    VERSION 5.6.201

--- Unity/Demos/Assets/DragonBones/Scripts/core/DragonBones.cs
+++ Unity/Demos/Assets/DragonBones/Scripts/core/DragonBones.cs
@@ -306,7 +306,7 @@ namespace DragonBones
         public static bool yDown = true;
         public static bool debug = false;
         public static bool debugDraw = false;
-        public static readonly string VERSION = "5.6.2";
+        public static readonly string VERSION = "5.6.201";
 
         private readonly WorldClock _clock = new WorldClock();
         private readonly List<EventObject> _events = new List<EventObject>();
